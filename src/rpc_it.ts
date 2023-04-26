4ffaed5f7c934662c81ccebc9d5130bed045a216
ahl5esoft <ahl5esoft@gmail.com>
2023-04-26 11:13:33 +0800

    docs: v3.5.0

--- src/rpc_it.ts
+++ src/rpc_it.ts
@@ -5,7 +5,7 @@ import { AxiosRpc as Self } from './rpc';
 describe('src/rpc.ts', () => {
     describe('.call<T>(opt: RpcCallOption)', () => {
         it('ok', async () => {
-            const res = await new Self('http://127.0.0.1').call<any>({
+            const res = await new Self('http://127.0.0.1:30000').call<any>({
                 route: '/aa/bb'
             });
             strictEqual(res.err, 501);
